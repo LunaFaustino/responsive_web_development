b4fe2b353a2e4f149c26c51758f5faf244ff4ce2
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-14 12:10:05 -0300

    Aula 14/03

---
+++ Segundo semestre/Aula1403/layout/src/app/Card/card.tsx
@@ -0,0 +1,25 @@
+
+const Card  = () =>{
+    return(
+        <>
+            <div className="content">
+                <div className="content-card">
+                    <div className="card">
+                        <h2>CARD</h2>
+                    </div>
+                    <div className="card">
+                        <h2>CARD</h2>
+                    </div>
+                    <div className="card">
+                        <h2>CARD</h2>
+                    </div>
+                    <div className="card">
+                        <h2>CARD</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card;
---
+++ Segundo semestre/Aula1403/layout/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Card from "./Card/card";
+import Carrossel from "./Carrossel/carrossel";
+
+export default function Home() {
+  return (
+    <>
+      <Carrossel/>
+      <Card/>
+    </>
+  );
+}
