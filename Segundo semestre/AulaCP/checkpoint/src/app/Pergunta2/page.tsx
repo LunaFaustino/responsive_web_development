5037bf7c5ea66222514b9c464f1d8173b5fdf1d9
LunaFaustino <lu.faustino2000@gmail.com>
2024-03-20 22:47:50 -0300

    Fazendo os useStates

--- Segundo semestre/AulaCP/checkpoint/src/app/Pergunta2/page.tsx
+++ Segundo semestre/AulaCP/checkpoint/src/app/Pergunta2/page.tsx
@@ -1,14 +1,24 @@
+"use client"
+import { SetStateAction, useState } from "react";
 
 const Pergunta2 = () => {
+
+    const [cor, setCor] = useState("");
+
+    const handleCorChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setCor(event.target.value);
+    }
+
     return(
         <>
             <h1>Escolha uma cor!</h1>
             <div>
                 <form>
-                    <input type="radio" name="cor" value="amarelo"/> Amarelo, me lembra o sol!
-                    <input type="radio" name="cor" value="meio"/> Acho que verde, tranquilo
-                    <input type="radio" name="cor" value="triste"/> Azul como o mar
+                    <input type="radio" name="cor" value="amarelo" onChange={handleCorChange}/> Amarelo, me lembra o sol!
+                    <input type="radio" name="cor" value="verde" onChange={handleCorChange}/> Acho que verde, tranquilo
+                    <input type="radio" name="cor" value="azul" onChange={handleCorChange}/> Azul como o mar
                 </form>
+                {cor && <p>Você selecionou a cor {cor}</p>}
             </div>
         </>
     )
