930a775a9e42baf1d4e2d666c31dcd5b773ab4cf
LunaFaustino <lu.faustino2000@gmail.com>
2024-03-22 18:41:12 -0300

    Feito o header

--- Segundo semestre/Salesforce/src/app/layout.tsx
+++ Segundo semestre/Salesforce/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Header from "./Header/header";
 
 export const metadata: Metadata = {
   title: "Salesforce",
@@ -13,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body>
+        <Header/>
         {children}
       </body>
     </html>
