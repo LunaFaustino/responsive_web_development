63b83b78b7b266eea223ce8cfc45ff40a699a6a1
LunaFaustino <lu.faustino2000@gmail.com>
2024-03-22 20:15:46 -0300

    Linkando a pagina

--- Segundo semestre/Salesforce/src/app/Header/header.tsx
+++ Segundo semestre/Salesforce/src/app/Header/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "../../../public/css/header.css"
 
 const Header = () =>{
@@ -7,6 +8,9 @@ const Header = () =>{
             <section className="header">
                 <img src="assets/logosalesforce.png" alt="" className="logosales"/>
                 <ul className="listaheader">
+                    <Link href="/Produtos">
+                        <li>Produtos</li>
+                    </Link>
                     <li>Produtos</li>
                     <li>Contato</li>
                     <li>Equipe</li>
