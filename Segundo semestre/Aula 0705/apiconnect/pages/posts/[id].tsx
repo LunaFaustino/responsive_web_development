94c9902dd6982701b66120cbfda3f8fb993c6361
LunaFaustino <lu.faustino2000@gmail.com>
2024-05-07 09:41:51 -0300

    Aula do dia 07/05

---
+++ Segundo semestre/Aula 0705/apiconnect/pages/posts/[id].tsx
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { fetchData } from "../../utils/api";
+
+
+interface Post{
+    userId : number;
+    id : number;
+    title : string;
+    body : string;
+}
+
+const PostPage = () => {
+
+    const router = useRouter();
+    const {id} = router.query;
+    const [post, setPost] = useState<Post | null>(null)
+
+    useEffect(() => {
+
+        const fetchPost = async() => {
+            if (id){
+                const data = await fetchData(`posts/${id}`);
+                if (data){
+                    setPost(data);
+                }
+            }
+        }
+        fetchPost();
+    },[id])
+    if (!post) {
+        return <div>Carregando...</div>
+    }
+
+    return (
+        <>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </>
+    )
+}
+
+export default PostPage;
